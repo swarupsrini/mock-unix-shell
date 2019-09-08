de88e50120a4f78ad375a827cdb6a23233765716
Swarup Srini <gswarup2001@gmail.com>
2019-09-08 18:43:47 -0400

    Add files via upload

---
+++ commands/UrlDataFetcher.java
@@ -0,0 +1,10 @@
+package commands;
+
+/**
+ * Represents classes used to obtain data from a URL.
+ * 
+ * @author Gnanaswarup Srinivasan
+ */
+public interface UrlDataFetcher {
+  public String getUrlData(String requestUrl);
+}
